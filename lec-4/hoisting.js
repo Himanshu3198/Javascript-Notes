a6ce66f2cb378e5eceec5c87f4e6747c53c7edf9
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-07-30 13:35:32 +0530

    Create hoisting.js

---
+++ lec-4/hoisting.js
@@ -0,0 +1,10 @@
+//  hoisting : we can use a variable before its declaration
+// 'use strict'
+let x;
+x=5;
+
+console.log(x);
+
+
+
+// use strict:  run faster than normal mode its more secure and it throws error that normal mode dont
