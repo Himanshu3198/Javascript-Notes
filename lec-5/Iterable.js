35e5f87f56183c78d9a11df220167d4984d10c0e
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2023-01-08 22:21:51 +0530

    Create Iterable.js

---
+++ lec-5/Iterable.js
@@ -0,0 +1,18 @@
+// const name="Himanshu";
+	// for(const x of name){
+	// 	console.log(x);
+	// }
+	// const letters=["a","b","c"];
+	// for(const x of letters){
+	// 	console.log(x);
+	// }
+	// iteration over set
+    const setItem=new Set(["a","b","c","d"]);
+	for(const x of setItem) console.log(x);
+	// for(let i=0;i<setItem.size;i++){
+	// 	console.log(setItem[i]);
+	// }
+	
+	// iteration over map
+	const fruits= new Map([["apples",200],["banana",100],["mango",300]]);
+	for(const x of fruits) console.log(x);
